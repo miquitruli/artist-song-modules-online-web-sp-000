53ea9c08050ae1576978373ab173a4b78112dc84
MICAELLA COLOMBO <micaelc@g.clemson.edu>
2020-08-06 20:07:09 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -20,6 +20,14 @@ class Song
     @@songs
   end
 
+  def self.reset_all
+    self.all.clear
+  end
+
+  def self.count
+    self.all.count
+  end
+
   def artist=(artist)
     @artist = artist
   end
