586b06b4722b631df6fcee0558c352b291be57b9
MICAELLA COLOMBO <micaelc@g.clemson.edu>
2020-08-06 19:47:35 +0000

    Automatically backed up by Learn

--- lib/concerns/memorable.rb
+++ lib/concerns/memorable.rb
@@ -1,14 +1,24 @@
-require 'pry'
+require "pry"
 
 module Memorable
+
   module ClassMethods
 
-    def self.reset_all
+    def reset_all
       self.all.clear
     end
 
-    def self.count
+    def count
       self.all.count
     end
+
   end
+
+  module InstanceMethods
+
+    def initialize
+      self.class.all << self
+    end
+  end
+
 end
